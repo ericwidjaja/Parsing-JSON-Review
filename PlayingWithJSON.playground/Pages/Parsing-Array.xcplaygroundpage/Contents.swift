b747a65f54e914fa1574f3ded56556ec5e5d9d72
Eric Widjaja <ericwidjaja@pursuit.org>
2020-07-29 18:37:03 -0400

    completed parsing array

--- PlayingWithJSON.playground/Pages/Parsing-Array.xcplaygroundpage/Contents.swift
+++ PlayingWithJSON.playground/Pages/Parsing-Array.xcplaygroundpage/Contents.swift
@@ -1,7 +1,9 @@
 import Foundation
 
-// JSON data
 
+//      ===================================================
+//                  JSON data
+//      ===================================================
 let json = """
 
 [
@@ -10,16 +12,48 @@ let json = """
         "location_type": "City",
         "woeid": 2459115,
         "latt_long": "40.71455,-74.007118"
+    },
+
+    {
+        "title": "Long Island City",
+        "location_type": "City",
+        "woeid": 2459115,
+        "latt_long": "40.740070,-73.935440"
     }
 ]
 
 """.data(using: .utf8)!
 
-// create model from JSON
+//          ===================================================
+//                      Create model(s)
+//          ===================================================
 
-struct City: Codable {
+
+struct City:  Codable {
     let title: String
-    let location_type: String
+//  let location_type: String // not a good Swift writing - no camel case
+//  Reminder - once property names are changed using the CodingKeys, they must match identically to the case type
+    let locationType: String
     let woeid: Int
-    let latt_long: String
+//    let latt_long: String // no camel case -> needs to be written better with CodingKeys
+    let coordinate: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case locationType = "location_type"
+        // change line 23 & 26 constant names to match their cases' names ---> "locationType" & "coordinate"
+        case woeid
+        case coordinate = "latt_long"
+    }
+}
+
+//          ===================================================
+//                  decode JSON to Swift objects
+//          ===================================================
+
+do {
+    let weatherArray = try JSONDecoder().decode([City].self, from: json)
+    dump(weatherArray)
+} catch {
+    print("decoding error: \(error)")
 }
