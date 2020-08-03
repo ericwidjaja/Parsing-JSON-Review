7a1b50b4f181ca91c51b0fb09c814e39efcab10a
Eric Widjaja <ericwidjaja@pursuit.org>
2020-08-03 12:11:54 -0400

    create project Parsing-JSON-Using-Bundle and make a presidents JSON file

---
+++ Parsing-JSON-Using-Bundle/Parsing-JSON-Using-Bundle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsing-JSON-Using-Bundle
+//
+//  Created by Eric Widjaja on 8/3/20.
+//  Copyright © 2020 ericW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
