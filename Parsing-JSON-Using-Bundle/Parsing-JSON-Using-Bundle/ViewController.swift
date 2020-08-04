aca729fda11754fe203b6d33e1eead8b5278d741
Eric Widjaja <ericwidjaja@pursuit.org>
2020-08-04 19:41:47 -0400

    made func configureDataSource, using diffableDataSource

--- Parsing-JSON-Using-Bundle/Parsing-JSON-Using-Bundle/ViewController.swift
+++ Parsing-JSON-Using-Bundle/Parsing-JSON-Using-Bundle/ViewController.swift
@@ -20,12 +20,28 @@ class ViewController: UIViewController {
     //both the SectionItemIdentifier and the ItemIdentifier need to conform to the Hasable Protocol
     
     typealias DataSource = UITableViewDiffableDataSource<Section, President> //to make it conform to Hashable, add 'Hashable' after 'Decodable' in the President.Swift Model
-    private var dataSource: DataSource!
+    private var dataSource: DataSource! //currently has nil value
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        configureDataSource()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    // https://www.youtube.com/watch?v=xkcl44jGRvQ at 1:01:26
+    private func configureDataSource() {                // similar to indexPath at RowAt
+    
+        dataSource = DataSource(tableView: presidentsTableView, cellProvider: { (presidentsTableView, indexPath, president) -> UITableViewCell? in
+            // configure cell
+            let cell = presidentsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = president.name
+            cell.detailTextLabel?.text = president.number.description
+            // or we can use string interpolation
+//            cell.detailTextLabel?.text = "\(president.number)"
+            return cell
+        })
     }
 
 
